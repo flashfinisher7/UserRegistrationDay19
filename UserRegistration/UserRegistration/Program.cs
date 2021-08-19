00c7755b4c6f2838b21cc72b107dd994df080b80
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-19 15:09:58 +0530

    Refactor: Validation For First Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,6 +7,10 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t\t\t User Registration Problem");
+            Registration user = new Registration();
+            Console.Write("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
         }
     }
 }
