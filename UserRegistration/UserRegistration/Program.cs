5449daa4441a0cb44345e230941b04a3839e3029
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-20 14:42:52 +0530

    Refactor: Upper Case

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,6 +7,22 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t\t\t User Registration Problem");
+            Registration user = new Registration();
+            Console.Write("Enter your first name:");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
+            Console.Write("Enter your last name:");
+            string lastName = Console.ReadLine();
+            user.Check(lastName);
+            Console.Write("Enter your Email id:");
+            string email = Console.ReadLine();
+            user.CheckMail(email);
+            Console.Write("Enter your Mobile Number:");
+            string num = Console.ReadLine();
+            user.PhoneNumberCheck(num);
+            Console.Write("Enter your Password:");
+            string psw = Console.ReadLine();
+            user.PasswordCheck(psw);
         }
     }
 }
