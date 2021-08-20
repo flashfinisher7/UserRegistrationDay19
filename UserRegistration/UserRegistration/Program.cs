58ba0a3ab04e2ba750bd68e818eb2b70165643d2
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-20 14:30:32 +0530

    Refactor: Checking phone Number Validation'

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,6 +7,20 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t\t\t User Registration Problem");
+            Registration user = new Registration();
+            Console.Write("Enter your First name:");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
+            Console.Write("Enter your Last name:");
+            string lastName = Console.ReadLine();
+            user.Check(lastName);
+            Console.Write("Enter your Email id:");
+            string email = Console.ReadLine();
+            user.CheckMail(email);
+            Console.Write("Enter your Mobile Number:");
+            string num = Console.ReadLine();
+            user.PhoneNumberCheck(num);
+
         }
     }
 }
