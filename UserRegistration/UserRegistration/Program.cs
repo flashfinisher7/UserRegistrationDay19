395a2f2356fc9ebd5def23b79c659384a7efd14e
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-20 14:21:12 +0530

    Refactor: Checking Email Valid or not

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -7,6 +7,17 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("\t\t\t\t\t\t User Registration Problem");
+            Registratoion user = new Registratoion();
+            Console.Write("Enter your First name:");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
+            Console.Write("Enter your Last name:");
+            string lastName = Console.ReadLine();
+            user.Check(lastName);
+            Console.Write("Enter your Email:");
+            string email = Console.ReadLine();
+            user.CheckMail(email);
+
         }
     }
 }
---
+++ UserRegistration/UserRegistration/Registration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class Registratoion
+    {
+        public void Check(string name)
+        {
+            //regex pattern is created to check validity
+            string pattern = "[A-Z][a-z]{3}[a-z]*";
+            Regex regex = new Regex(pattern);
+            // IsMatch method check the pattern and name
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine("Name {0} is Valid ", name);
+            }
+            else
+            {
+                Console.WriteLine("Name {0} is Invalid", name);
+            }
+        }
+        public void CheckMail(string mail)
+        {
+            //regex pattern is created to check validity
+            string pattern = @"^[a-z]{3}([\# \+ _\.]*[a-zA-Z0-9])*@[a-zA-z]+\.[a-z]{2,3}(\.[a-zA-Z]{2,4}){0,1}$";
+            Regex regex = new Regex(pattern);
+            // IsMatch method check the pattern and mail
+            if (regex.IsMatch(mail))
+            {
+                Console.WriteLine("Email :{0} is Valid", mail);
+            }
+            else
+            {
+                Console.WriteLine("Email :{0} is Invalid", mail);
+            }
+        }
+    }
+}
